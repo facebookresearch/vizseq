4a0b6db20bf13c5de32171c330fa2957a42d5943
Changhan Wang <changhan@fb.com>
2019-11-04 11:26:06 +0800

    update example data downloading script

--- get_example_data.sh
+++ get_example_data.sh
@@ -3,6 +3,6 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FILENAME=example_data.zip
-wget https://dl.fbaipublicfiles.com/vizseq/${FILENAME} -O "${ROOT}/examples/${FILENAME}"
+curl "https://dl.fbaipublicfiles.com/vizseq/${FILENAME}" --output "${ROOT}/examples/${FILENAME}"
 unzip "${ROOT}/examples/${FILENAME}" -d "${ROOT}/examples"
 rm "${ROOT}/examples/${FILENAME}"
