cced17579cf7d062d6758ae4b131bcf083330943
Changhan Wang <changhan@fb.com>
2019-11-10 20:54:26 +0800

    update landing page, examples and docs

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -8,6 +8,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
+import CodeBlock from '@theme/CodeBlock';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
@@ -41,6 +42,18 @@ const features = [
   },
 ];
 
+const ipynbDataInputCode = `from glob import glob
+root = 'examples/data/translation_wmt14_en_de_test'
+src, ref, hypo = glob(f'{root}/src_*.txt'), glob(f'{root}/ref_*.txt'), glob(f'{root}/pred_*.txt')
+`;
+
+const ipynbViewingCode = `import vizseq
+vizseq.view_stats(src, ref)
+vizseq.view_n_grams(src)
+vizseq.view_scores(ref, hypo, ['bleu', 'meteor'])
+vizseq.view_examples(src, ref, hypo, ['bleu', 'meteor'], query='book', page_sz=10, page_no=1)
+`;
+
 function Feature({imageUrl, title, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
@@ -73,7 +86,7 @@ function Home() {
                 'button button--success button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/installation')}>
+              to="#quickstartSection">
               Get Started
             </Link>
           </div>
@@ -91,6 +104,28 @@ function Home() {
             </div>
           </section>
         )}
+        <section id="quickstartSection" className={classnames('hero', styles.quickstart)}>
+            <div className="container">
+                <h1 className="text--center">Quickstart</h1>
+                <h4>Install VizSeq:</h4>
+                <CodeBlock className="bash">$ pip install vizseq</CodeBlock>
+                <br/>
+                <h4>Use VizSeq in Jupyter notebook:</h4>
+                <h9>First, set up data inputs:</h9>
+                <CodeBlock className="python">{ipynbDataInputCode}</CodeBlock>
+                <br/>
+                <h9>Then:</h9>
+                <CodeBlock className="python">{ipynbViewingCode}</CodeBlock>
+                <br/>
+                <h4>Or use VizSeq Web App:</h4>
+                <CodeBlock className="bash">$ python -m vizseq.server --port 9001 --data-root examples/data</CodeBlock>
+                <br/>
+                <h9>In your web browser, navigate to:</h9>
+                <CodeBlock className="bash">http://localhost:9001</CodeBlock>
+                <br/>
+                <h4>For more details, please check out the <Link to={useBaseUrl('docs/overview')}>Docs</Link>.</h4>
+            </div>
+        </section>
       </main>
     </Layout>
   );
