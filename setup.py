8b6645572ea59cf957e77bfca455f1ef1da287a4
Changhan Wang <changhan@fb.com>
2019-11-12 00:49:56 -0800

    update setup.py (#18)

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
